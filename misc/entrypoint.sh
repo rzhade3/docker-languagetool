29d2e2b48310cf221bb72a655c05701c112ebfe3
Rahul Zhade <rzhade3@github.com>
2020-05-18 01:02:38 -0700

    Renaming variables consistently

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -4,8 +4,8 @@ LANGUAGE=$1 # Specified language in workflow file
 FILEPATH=$2 # filepath specified in workflow file
 
 # Since the cloned repo is in the workspace directory, need to get a path there
-PATH_IN_CONTAINER="$GITHUB_WORKSPACE/$FILE_TO_PARSE"
+PATH_IN_CONTAINER="$GITHUB_WORKSPACE/$FILEPATH"
 LOGFILE="$(date +%s).txt"
 
 java -jar /LanguageTool/languagetool-commandline.jar -l $1 --json $PATH_IN_CONTAINER >> $LOGFILE
-python3 /parse_log.py $FILE_TO_PARSE $LOGFILE
+python3 /parse_log.py $FILEPATH $LOGFILE
