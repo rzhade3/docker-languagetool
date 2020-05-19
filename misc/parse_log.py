9e5bb9feba2bf0f04dc7295958d018dd9dabd815
Rahul Zhade <rzhade3@github.com>
2020-05-18 22:35:25 -0700

    Added logic to parse line and column

--- misc/parse_log.py
+++ misc/parse_log.py
@@ -1,8 +1,24 @@
+import os
 import sys
 import json
 
 warning_message = u'::warning file={},line={},col={}::{}'
 suggestion_message = u'{} Suggestions: `{}`'
+
+def get_line_and_column(filename, offset):
+	github_workspace = os.environ['GITHUB_WORKSPACE']
+	filepath = os.path.join(github_workspace, filename)
+	with open(filepath, 'r', encoding='UTF-8') as f:
+		column = 1
+		line = 1
+		for i in range(offset):
+			if f.read(1) == '\n':
+				line += 1
+				column = 1
+			else:
+				column += 1
+		return line, column
+
 
 parsed_filename = sys.argv[1]
 logfile = sys.argv[2]
@@ -20,5 +36,6 @@ with open(logfile, 'r', encoding='UTF-8') as f:
 		replacement_suggestions = list(map(lambda replacement: replacement["value"], match["replacements"]))
 		replacement_string = "; ".join(replacement_suggestions)
 		msg = suggestion_message.format(match["message"], replacement_string)
-		warning = warning_message.format(parsed_filename, 1, match["offset"], msg)
+		line, column = get_line_and_column(parsed_filename, match["offset"])
+		warning = warning_message.format(parsed_filename, line, column, msg)
 		print(warning)
